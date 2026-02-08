6c1ac293f5dc1f397a1d1a1e408ea3cd47857a46
Victor Garcia <79111937+chimbaaNg@users.noreply.github.com>
2026-02-08 01:42:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -61,30 +61,6 @@ function actualizarCuentaAtras() {
 setInterval(actualizarCuentaAtras, 1000);
 actualizarCuentaAtras();
 
-const audio = document.getElementById("audio");
-const playPauseBtn = document.getElementById("playPauseBtn");
-const tiempoAudio = document.getElementById("tiempo-audio");
-
-playPauseBtn.addEventListener("click", () => {
-    if (audio.paused) {
-        audio.play();
-        playPauseBtn.textContent = "⏸️";
-    } else {
-        audio.pause();
-        playPauseBtn.textContent = "▶️";
-    }
-});
-
-audio.addEventListener("timeupdate", () => {
-    const minActual = Math.floor(audio.currentTime / 60);
-    const secActual = Math.floor(audio.currentTime % 60).toString().padStart(2, "0");
-
-    const minTotal = Math.floor(audio.duration / 60) || 0;
-    const secTotal = Math.floor(audio.duration % 60).toString().padStart(2, "0");
-
-    tiempoAudio.textContent = `${minActual}:${secActual} / ${minTotal}:${secTotal}`;
-});
-
 
 // ===========================
 // DESBLOQUEO DEL PRESAVE
@@ -115,4 +91,5 @@ presaveBtn.addEventListener("click", (e) => {
         e.preventDefault(); // bloqueado → no hace nada
     }
 });
+
 

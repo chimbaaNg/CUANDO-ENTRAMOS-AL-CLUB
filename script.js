d921dbeb11f509c86bb3835883408dc3ea415933
Victor Garcia <79111937+chimbaaNg@users.noreply.github.com>
2026-02-08 02:24:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -61,37 +61,7 @@ function actualizarCuentaAtras() {
 setInterval(actualizarCuentaAtras, 1000);
 actualizarCuentaAtras();
 
-// ===========================
-// AUDIO PLAY / PAUSE + TIEMPO
-// ===========================
 
-const audio = document.getElementById("audio");
-const playPauseBtn = document.getElementById("playPauseBtn");
-const tiempoAudio = document.getElementById("tiempo-audio");
-
-playPauseBtn.addEventListener("click", () => {
-    if (audio.paused) {
-        audio.play();
-        playPauseBtn.textContent = "⏸️";
-    } else {
-        audio.pause();
-        playPauseBtn.textContent = "▶️";
-    }
-});
-
-audio.addEventListener("timeupdate", () => {
-    const minActual = Math.floor(audio.currentTime / 60);
-    const secActual = Math.floor(audio.currentTime % 60).toString().padStart(2, "0");
-
-    const minTotal = Math.floor(audio.duration / 60) || 0;
-    const secTotal = Math.floor(audio.duration % 60).toString().padStart(2, "0");
-
-    tiempoAudio.textContent = `${minActual}:${secActual} / ${minTotal}:${secTotal}`;
-});
-
-audio.addEventListener("ended", () => {
-    playPauseBtn.textContent = "▶️";
-});
 
 
 
